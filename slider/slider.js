a5d88e6973b249ec67063674aff78e4903ede97e
Lealgore <apple@Mac-a8206613224d.home>
2016-04-17 21:40:05 -0500

    sitio version1
    Sitio HTML

---
+++ slider/slider.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+	$('.bxslider').bxSlider({
+		mode: 'fade',
+		slideWidth: 250,
+		maxSlides: 3,
+		mode: 'horizontal',
+		captions: true,
+		adaptiveHeight: true
+	});
+});
